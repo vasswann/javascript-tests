fa1aed0b348db135c3937d85143e497720cc9983
Istvan Vas <swannxxx@yahoo.co.uk>
2020-12-16 22:28:52 +0000

    Flatten-array

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,15 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5],
+        flatArr = [];
+
+        // convert to a string, split with come then sort
+        // const arrayToString = arr.toString();
+        // flatArr = arrayToString.split(',').sort((a, b)=> a - b);
+
+        //ES2019 Array.flat() method
+        flatArr = arr.flat(Infinity).sort((a, b)=> a - b);
 
     expect(arr).toEqual(expected);
   });
