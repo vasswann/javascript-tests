eadca24fcf7cdb82545b9dccaea458b63e6c5803
Istvan Vas <swannxxx@yahoo.co.uk>
2020-12-16 21:58:12 +0000

    Clone-object

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,6 +3,15 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
+        // deep cloan with JSON
+        // obj = JSON.parse(JSON.stringify(expected));
+
+        // or shallow copy of the object with assign()
+        // obj = Object.assign({}, expected)
+
+        // shallow copy with spread operator. Most of the time I use spread in React
+        obj = {...expected, skills:[...expected.skills]}
+
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
